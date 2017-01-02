1d9d6d8d87b0f01fa453c8f63d3d7c6e9ba17578
Abigail Iheke <aiheke@ucvts.org>
2017-01-02 18:34:54 +0000

    moved files to different folders

---
+++ api-test.py
@@ -0,0 +1,49 @@
+import requests
+
+import csv
+
+csvf = open('pokemon.csv', 'w')
+csvw = csv.writer(csvf, delimiter=',')
+f = open('location.csv', 'w')
+w = csv.writer(f, delimiter=',')
+
+
+csvw.writerow(['Location','Pokemon'])
+w.writerow(['Location','# of Pokemon'])
+
+endpoint = 'http://pokeapi.co/api/v2/'
+
+
+location = 'pokemon-habitat/'
+
+url = endpoint + location
+
+r = requests.get(url)
+print(r.url)
+
+data = r.json()
+
+
+pokehabitat = data['results']
+
+y = 0
+for habitat in pokehabitat:
+    print(habitat['name'])
+    print(habitat['url'])
+    
+    x = 0
+    
+    url1 = habitat['url']
+    p = requests.get(url1)
+    data1 = p.json()
+    pokemon =  data1['pokemon_species']
+    for name in pokemon:
+        csvw.writerow([habitat['name'],name['name']])
+        x += 1
+    w.writerow([habitat['name'],x])
+    y += x
+
+w.writerow(['Total',y])
+
+csvf.close()
+f.close()
--- makelist.py
+++
@@ -1,6 +0,0 @@
-f = open('whoupclicklike.py', 'w')
-
-for x in range(10):
-    f.write(str(x + 1) + '. ' + input() + '\n')
-    
-f.close()
